9bd9edc7a7ad3c3bf613c1aee9caca8b7cec705d
Beau Davenport <beau.davenport@asynchrony.com>
2018-12-19 21:34:23 -0600

    Add markdown support to display sample blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,14 @@ module.exports = {
     title: 'Gatsby Default Starter',
   },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: 'markdown-pages',
+      },
+    },
+    'gatsby-transformer-remark',
     'gatsby-plugin-netlify-cms',
     'gatsby-plugin-react-helmet',
     {
