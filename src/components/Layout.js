5130d3a946093cd4fb2aeab513144effecb2e93d
Beau Davenport <beau.davenport@asynchrony.com>
2019-01-09 21:31:45 -0600

    Add partial navigation drawer, add material icons font - BD

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -2,40 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import { StaticQuery, graphql } from 'gatsby';
-import { NavigationDrawer } from 'react-md';
+import { NavigationDrawer, FontIcon } from 'react-md';
 import Header from './header';
 import './layout.scss';
 
-class Layout extends React.Component {
-  // state = {
-  //   scrolledEnough: false,
-  // }
-  //
-  // componentDidMount() {
-  //   console.log('mounted')
-  //   window.addEventListener('scroll', this.handleScrollToElement)
-  // }
-  //
-  // componentWillUnmount() {
-  //   console.log('UNmounted')
-  //   window.removeEventListener('scroll', this.handleScrollToElement)
-  // }
-  //
-  // handleScrollToElement = event => {
-  //   console.log('Fired', event.currentTarget.scrollY)
-  //   if (!this.state.scrolledEnough && event.currentTarget.scrollY > 50) {
-  //     this.setState({ scrolledEnough: true })
-  //   }
-  //   if (this.state.scrolledEnough && event.currentTarget.scrollY < 50) {
-  //     this.setState({ scrolledEnough: false })
-  //   }
-  // }
-
-  render() {
-    const { children } = this.props;
-    return (
-      <StaticQuery
-        query={graphql`
+const Layout = ({ children }) => (
+  <StaticQuery
+    query={graphql`
           query SiteTitleQuery {
             site {
               siteMetadata {
@@ -44,35 +17,40 @@ class Layout extends React.Component {
             }
           }
         `}
-        render={data => (
-          <NavigationDrawer>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Sample' },
-                { name: 'keywords', content: 'sample, something' },
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            <Header
-              siteTitle={data.site.siteMetadata.title}
-            />
-            <div>
-              <div
-                style={{
-                  margin: 'auto',
-                }}
-              >
-                {children}
-              </div>
-            </div>
-          </NavigationDrawer>
-        )}
-      />
-    );
-  }
-}
+    render={data => (
+      <NavigationDrawer
+        desktopDrawerType={NavigationDrawer.DrawerTypes.FULL_HEIGHT}
+        tabletDrawerType={NavigationDrawer.DrawerTypes.TEMPORARY}
+        mobileDrawerType={NavigationDrawer.DrawerTypes.TEMPORARY}
+        position="left"
+        defaultVisible={false}
+        temporaryIcon={(<FontIcon iconClassName="material-icons">menu</FontIcon>)}
+      >
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        >
+          <html lang="en" />
+        </Helmet>
+        <Header
+          siteTitle={data.site.siteMetadata.title}
+        />
+        <div>
+          <div
+            style={{
+              margin: 'auto',
+            }}
+          >
+            {children}
+          </div>
+        </div>
+      </NavigationDrawer>
+    )}
+  />
+);
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
