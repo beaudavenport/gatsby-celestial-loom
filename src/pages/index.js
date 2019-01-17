aba3d8f697b3cec0276b1ba156c23714cec419e4
Beau Davenport <beau.davenport@asynchrony.com>
2019-01-16 21:55:01 -0600

    Fleshing out homescreen cards for posts and events - BD

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {
-  Paper, Grid, Cell, Button, Card, CardTitle, CardText,
+  Paper, Grid, Cell, Button, Card, CardTitle, CardText, MediaOverlay,
 } from 'react-md';
 import Layout from '../components/Layout';
 import Stars from '../images/astrology-sunscreen.jpg';
+import Preview from '../images/2019-Cosmic-Preview.jpg';
+import Pleiades from '../images/pleiades.jpg';
 
 const IndexPage = () => (
   <Layout>
@@ -45,26 +47,74 @@ Astrological Services
       </Grid>
     </Paper>
     <Grid>
-      <h1>Home</h1>
+      <Cell>
+        <h1 style={{ fontStyle: 'italic' }}>Latest Post</h1>
+      </Cell>
       <Cell size={12}>
         <Card style={{ backgroundColor: 'white' }}>
-          <CardTitle title="Crossroads in 2019" subtitle="01/12/19" />
+          <div style={{ position: 'relative' }}>
+            <img style={{ height: '300px', width: '100%', objectFit: 'cover' }} src={Preview} alt="Preview of feature post" />
+            <MediaOverlay style={{ marginBottom: '5px' }}>
+              <CardTitle title="The Celestial Loom Year 2019 Cosmic Preview" subtitle="12/24/18" />
+            </MediaOverlay>
+          </div>
           <CardText>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit lacus nec velit bibendum, fringilla venenatis tortor pulvinar. Nam rhoncus molestie luctus. Duis ac ultricies velit. Aliquam tempor leo ut mattis consectetur. Mauris id ex vel dolor faucibus vehicula. Praesent volutpat, orci id maximus consectetur, neque magna dignissim urna, hendrerit consectetur diam lectus a lorem. Maecenas ultricies feugiat dui in dictum. Donec mattis tincidunt ligula, eget pretium purus blandit vitae. Nam mattis vel magna volutpat luctus. Etiam blandit dui in tellus lacinia, quis congue urna lacinia. Nullam lectus nibh, rhoncus non volutpat ac, auctor in turpis. Nullam tellus elit, venenatis.</p>
           </CardText>
         </Card>
       </Cell>
-    </Grid>
-    <Grid>
-      <Cell size={6}>
-        <Paper style={{ backgroundColor: 'white' }}>
-          <h1>Event</h1>
-        </Paper>
+      <Cell size={12}>
+        <Card style={{ backgroundColor: 'white' }}>
+          <div style={{ flexDirection: 'row', display: 'flex' }}>
+            <div style={{ }}>
+              <img style={{ marginBottom: '-5px', objectFit: 'cover' }} src={Pleiades} alt="Preview of next post" />
+            </div>
+            <div style={{ padding: '20px' }}>
+              <p style={{ fontSize: 'large' }}>Getting started with astrology</p>
+              <p style={{ fontSize: 'medium' }}>07/30/16</p>
+              <CardText>
+                <p>
+                  It can be difficult to get started with astrology.
+                  There can be many resources to understand and it consultant
+                  be difficult to know what is reliable.
+                </p>
+              </CardText>
+            </div>
+          </div>
+        </Card>
       </Cell>
-      <Cell size={6}>
-        <Paper style={{ backgroundColor: 'white' }}>
-          <h1>Service</h1>
-        </Paper>
+      <Cell>
+        <h1 style={{ fontStyle: 'italic' }}>Upcoming Events</h1>
+      </Cell>
+      <Cell size={12}>
+        <Card style={{ backgroundColor: 'white' }}>
+          <div style={{ flexDirection: 'row', display: 'flex' }}>
+            <div style={{ backgroundColor: 'rgb(255, 128, 19)', padding: '20px 10px' }}>
+              <p style={{
+                fontStyle: 'italic', fontSize: '60px', textAlign: 'center', color: 'white', marginBottom: '30px',
+              }}
+              >
+15
+              </p>
+              <p style={{
+                fontStyle: 'italic', fontSize: '30px', textAlign: 'center', color: 'white',
+              }}
+              >
+JUN
+              </p>
+            </div>
+            <div style={{ padding: '20px' }}>
+              <p style={{ fontSize: 'large' }}>Neptune/Uranus Square PlayShop</p>
+              <p style={{ fontSize: 'medium' }}>Divine Inspirations Boutique</p>
+              <CardText>
+                <p>
+                  The Square of the Century is upon us! Join us for a magical
+                  PlayShop where we can discuss what this event means for you
+                </p>
+              </CardText>
+            </div>
+          </div>
+        </Card>
       </Cell>
     </Grid>
   </Layout>
