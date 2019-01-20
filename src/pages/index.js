6921d7a316fe0e933be89271c3f63636b6a5e3f3
Beau Davenport <beau.davenport@asynchrony.com>
2019-01-20 13:42:00 -0600

    Make thumbnail card image a background image - BD

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  Paper, Grid, Cell, Button, Card, CardTitle, CardText, MediaOverlay,
+  Paper, Grid, Cell, Button, Card, CardTitle, CardText, MediaOverlay, FontIcon,
 } from 'react-md';
 import { Link } from 'gatsby';
 import Layout from '../components/Layout';
@@ -58,26 +58,24 @@ Astrological Services
               </MediaOverlay>
             </div>
             <CardText>
-              <p style={{ textDecoration: 'none' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit lacus nec velit bibendum, fringilla venenatis tortor pulvinar. Nam rhoncus molestie luctus. Duis ac ultricies velit. Aliquam tempor leo ut mattis consectetur. Mauris id ex vel dolor faucibus vehicula. Praesent volutpat, orci id maximus consectetur, neque magna dignissim urna, hendrerit consectetur diam lectus a lorem. Maecenas ultricies feugiat dui in dictum. Donec mattis tincidunt ligula, eget pretium purus blandit vitae. Nam mattis vel magna volutpat luctus. Etiam blandit dui in tellus lacinia, quis congue urna lacinia. Nullam lectus nibh, rhoncus non volutpat ac, auctor in turpis. Nullam tellus elit, venenatis.</p>
+              <p style={{ textDecoration: 'none' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam suscipit lacus nec velit bibendum, fringilla venenatis tortor pulvinar. Nam rhoncus molestie luctus. Duis ac ultricies velit. Aliquam tempor leo ut mattis consectetur. Mauris id ex vel dolor faucibus vehicula. Praesent volutpat, orci id maximus consectetur, neque magna dignissim urna, hendrerit consectetur diam lectus a lorem. Maecenas ultricies feugiat dui in dictum. Donec mattis tincidunt ligula, eget pretium purus blandit vitae. Nam mattis vel magna volutpat luctus.</p>
             </CardText>
           </Card>
         </Link>
         <h1 style={{ fontStyle: 'italic' }}>Earlier Articles</h1>
         <Card style={{ backgroundColor: 'white' }}>
           <div style={{ flexDirection: 'row', display: 'flex' }}>
-            <div style={{ }}>
-              <img style={{ marginBottom: '-5px', objectFit: 'cover' }} src={Pleiades} alt="Preview of next post" />
-            </div>
-            <div style={{ padding: '20px' }}>
-              <p style={{ fontSize: 'large' }}>Getting started with astrology</p>
-              <p style={{ fontSize: 'medium' }}>07/30/16</p>
-              <CardText>
-                <p>
-                    It can be difficult to get started with astrology.
-                    There can be many resources to understand and it consultant
-                    be difficult to know what is reliable.
-                </p>
-              </CardText>
+            <div style={{
+              height: '125px',
+              width: '125px',
+              background: `url(${Pleiades})`,
+              backgroundPosition: 'center',
+              backgroundSize: 'cover',
+            }}
+            />
+            <div style={{ padding: '10px' }}>
+              <h4 style={{ fontWeight: 'bold' }}>Getting started with astrology</h4>
+              <caption>07/30/16</caption>
             </div>
           </div>
         </Card>
@@ -102,7 +100,10 @@ Astrological Services
             </div>
             <div style={{ padding: '10px' }}>
               <p style={{ fontSize: '12px', fontWeight: 'bold' }}>Neptune/Uranus Square PlayShop</p>
-              <p style={{ fontSize: '10px' }}>Divine Inspirations Boutique</p>
+              <div style={{ display: 'flex', justifyContent: 'baseline' }}>
+                <FontIcon iconClassName="material-icons" style={{ marginBottom: '-5px' }}>near_me</FontIcon>
+                <p style={{ fontSize: '10px' }}>Divine Inspirations Boutique</p>
+              </div>
             </div>
           </div>
         </Card>
