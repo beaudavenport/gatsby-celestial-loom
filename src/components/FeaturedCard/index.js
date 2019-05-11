ed83b23824a731e0d958555a8b6db08cc6278156
Beau Davenport <beau.davenport@asynchrony.com>
2019-05-11 13:46:35 -0500

    Move chips down to prevent nested links; hide touchable link highlight on mobile - BD

--- src/components/FeaturedCard/index.js
+++ src/components/FeaturedCard/index.js
@@ -11,33 +11,35 @@ import RelatedItemChip from '../RelatedItemChip';
 const FeaturedCard = ({
   path, image, title, subtitle, excerpt, style, publishDate, cornerIconName, cornerTitle, relatedItems = [],
 }) => (
-  <TouchableLink to={path} style={style}>
-    <div style={{ position: 'relative' }}>
-      <img style={{ height: '250px', width: '100%', objectFit: 'cover' }} src={image} alt="Preview of featured post" />
-      {cornerIconName && <CardCornerHighlight iconName={cornerIconName} title={cornerTitle} />}
-    </div>
-    <div style={{ padding: 16 }}>
-      <Title>{title}</Title>
-      <h3 style={{
-        color: 'rgba(0, 0, 0, 0.54)', fontWeight: 'bold', marginBottom: 0, marginLeft: 5, fontSize: '1.1rem',
-      }}
-      >
-        {publishDate}
-      </h3>
-    </div>
+  <div>
+    <TouchableLink to={path} style={style}>
+      <div style={{ position: 'relative' }}>
+        <img style={{ height: '250px', width: '100%', objectFit: 'cover' }} src={image} alt="Preview of featured post" />
+        {cornerIconName && <CardCornerHighlight iconName={cornerIconName} title={cornerTitle} />}
+      </div>
+      <div style={{ padding: 16 }}>
+        <Title>{title}</Title>
+        <h3 style={{
+          color: 'rgba(0, 0, 0, 0.54)', fontWeight: 'bold', marginBottom: 0, marginLeft: 5, fontSize: '1.1rem',
+        }}
+        >
+          {publishDate}
+        </h3>
+      </div>
+      <CardText style={{ padding: 16 }}>
+        <p style={{ textDecoration: 'none' }}>
+          {excerpt}
+        </p>
+        <TextWithChevron text="Read more" />
+      </CardText>
+    </TouchableLink>
     <div style={{ padding: 10 }}>
       <p style={{ color: '#ec6602', fontWeight: 'bold' }}>Mentioned in this post:</p>
       <div style={{ display: 'flex', justifyContent: 'flex-start', flexWrap: 1 }}>
         {Boolean(relatedItems) && relatedItems.map(item => (<RelatedItemChip style={{ margin: 10 }} title={item} />))}
       </div>
     </div>
-    <CardText style={{ padding: 16 }}>
-      <p style={{ textDecoration: 'none' }}>
-        {excerpt}
-      </p>
-      <TextWithChevron text="Read more" />
-    </CardText>
-  </TouchableLink>
+  </div>
 );
 
 FeaturedCard.propTypes = {
