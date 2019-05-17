ef97b6dc67d37ac95e87d8c2cf335c71d16e9461
Beau Davenport <beau.davenport@asynchrony.com>
2019-05-17 18:12:00 -0500

    Add asterisks for req. fields, add birth location - BD

--- src/components/AddToCartButton/index.js
+++ src/components/AddToCartButton/index.js
@@ -15,16 +15,18 @@ const AddToCartButton = ({
     data-item-price={price}
     data-item-description={description}
     data-item-url={slug}
-    data-item-custom1-name="Name"
+    data-item-custom1-name="Name*"
     data-item-custom1-required="true"
-    data-item-custom2-name="Month of Birth"
+    data-item-custom2-name="Month of Birth*"
     data-item-custom2-required="true"
-    data-item-custom3-name="Day of Birth"
+    data-item-custom3-name="Day of Birth*"
     data-item-custom3-required="true"
-    data-item-custom4-name="Year of Birth"
+    data-item-custom4-name="Year of Birth*"
     data-item-custom4-required="true"
-    data-item-custom5-name="Time of Birth (if known)"
+    data-item-custom5-name="Time of Birth, if known"
     data-item-custom5-required="false"
+    data-item-custom6-name="Birth Location (City, State, Country), if known"
+    data-item-custom6-required="false"
   >
     Add to Cart
   </Button>
