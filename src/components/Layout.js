7163833d0a1a721a0b3a6337294e0b721f204c2e
Beau Davenport <beau.davenport@asynchrony.com>
2019-05-21 22:37:53 -0500

    Update snipcart api key to live version - BD

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -91,7 +91,7 @@ const Layout = ({
         >
           <html lang="en" />
           <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.2/jquery.min.js" />
-          <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="Y2NiNmUxNzEtMGNlZS00NTlkLTg1NDEtZjJjMGRjMWNjZThjNjM2ODU2Njg0NDE5MzU2MDg3" id="snipcart" />
+          <script src="https://cdn.snipcart.com/scripts/2.0/snipcart.js" data-api-key="YjQ4NTMxNDAtYWU5NS00MjRkLTliNjItYzU4ZWM3NDM3Mzk5NjM2ODU2Njg0NDE5MzU2MDg3" id="snipcart" />
           <link href="https://cdn.snipcart.com/themes/2.0/base/snipcart.min.css" rel="stylesheet" type="text/css" />
         </Helmet>
         {jumbotron}
