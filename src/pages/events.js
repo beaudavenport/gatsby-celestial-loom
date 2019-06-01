4fe6f6c18bd1f31e13946d6d7762ee5721bd4c9d
Beau Davenport <beau.davenport@asynchrony.com>
2019-06-01 15:11:51 -0500

    Split upcoming and past events on events page, add headers - BD

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,9 +1,11 @@
 import { CardText, Cell, Grid } from 'react-md';
 import { graphql } from 'gatsby';
+import { partition } from 'lodash';
 import PropTypes from 'prop-types';
 import React, { Fragment } from 'react';
+import moment from 'moment';
 
-import { BackLink, BigSubheader } from '../components/Common';
+import { BackLink, BigSubheader, JumboSubheader } from '../components/Common';
 import EventsArchive from '../components/EventsArchive';
 import FeaturedEventCard from '../components/FeaturedEventCard';
 import Layout from '../components/Layout';
@@ -11,6 +13,10 @@ import SidebarContents from '../components/SidebarContents';
 
 const Events = ({ data }) => {
   const nodes = data.allMarkdownRemark.edges.map(edge => edge.node);
+  const [pastEvents, upcomingEvents] = partition(
+    nodes,
+    node => moment().isAfter(moment(node.frontmatter.eventDate)),
+  );
 
   return (
     <Layout
@@ -25,12 +31,29 @@ const Events = ({ data }) => {
       <BackLink to="/" title="Home" />
       <Grid>
         <Cell size={12}>
-          <CardText>
-            <BigSubheader>Join us in-person!</BigSubheader>
+          <div style={{ marginBottom: 30 }}>
+            <JumboSubheader>Join us in-person!</JumboSubheader>
             <p style={{ fontWeight: 'bold' }}>Nikki provides playshops, speaking engagements, and more around the St. Louis area.</p>
-          </CardText>
+          </div>
         </Cell>
-        {nodes.map(node => (
+        <BigSubheader>Upcoming Events</BigSubheader>
+        {upcomingEvents.map(node => (
+          <Cell size={12} style={{ padding: '20px 0px' }}>
+            <FeaturedEventCard
+              path={node.fields.slug}
+              title={node.frontmatter.title}
+              eventDate={node.frontmatter.eventDate}
+              eventTime={node.frontmatter.eventTime}
+              eventDateShort={node.frontmatter.eventDateShort}
+              image={node.frontmatter.image}
+              eventPrice={node.frontmatter.eventPrice}
+              priceDescription={node.frontmatter.priceDescription}
+              location={node.frontmatter.location}
+            />
+          </Cell>
+        ))}
+        <BigSubheader>Past Events</BigSubheader>
+        {pastEvents.map(node => (
           <Cell size={12} style={{ padding: '20px 0px' }}>
             <FeaturedEventCard
               path={node.fields.slug}
